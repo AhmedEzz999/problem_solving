c507af15a7424ff7262d5b0ca2b4867390c6b55e
Ahmed Ezz <ahmedezz9877@gmail.com>
2025-09-03 08:01:47 +0300

    Solve Problem 1

--- problem_1/lib/main.dart
+++ problem_1/lib/main.dart
@@ -1,2 +1,13 @@
-// problem link:
-void main() {}
+// problem link: https://www.codewars.com/kata/5715eaedb436cf5606000381
+void main() {
+  List<int> numbersList = [1, -4, 7, 12, -5, 5, -10];
+  print(positiveSum(numbersList)); // Output is 25
+}
+
+int positiveSum(List<int> arr) {
+  final List<int> positiveNumbers = arr.where((number) => number > 0).toList();
+  if (positiveNumbers.isEmpty) {
+    return 0;
+  }
+  return positiveNumbers.reduce((a, b) => a + b);
+}
