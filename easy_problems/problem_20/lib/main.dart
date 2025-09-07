6971624ff4a3c3bc4405171675addfa1e4e5f7b5
Ahmed Ezz <ahmedezz9877@gmail.com>
2025-09-08 01:33:01 +0300

    Solve Problem 20

---
+++ easy_problems/problem_20/lib/main.dart
@@ -0,0 +1,9 @@
+// problem link: https://www.codewars.com/kata/55a70521798b14d4750000a4
+
+void main() {
+  print(
+    greet('Ahmed Ezz'),
+  ); // Output is Hello, Ahmed Ezz how are you doing today?
+}
+
+String greet(String name) => "Hello, $name how are you doing today?";
