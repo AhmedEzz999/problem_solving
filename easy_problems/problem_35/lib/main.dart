0fc2c87e646d0440a0dffeea927c5e1752c9bf85
Ahmed Ezz <ahmedezz9877@gmail.com>
2025-09-20 23:59:55 +0300

    Solve Problem 35

---
+++ easy_problems/problem_35/lib/main.dart
@@ -0,0 +1,11 @@
+// problem link: https://www.codewars.com/kata/56541980fa08ab47a0000040
+
+void main() {
+  print(printerError('aaaxbbbbyyhwawiwjjjwwm')); // Output is 8/22
+}
+
+String printerError(String string) {
+  final regex = RegExp(r'[a-m]');
+  final List errorList = regex.allMatches(string).toList();
+  return (1 - (errorList.length / string.length)).toString();
+}
